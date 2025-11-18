4568f77db11825af6372b9f57f6f739b28dcd9b4
catomaior <pietro.firpo@pm.me>
2025-11-18 11:54:20 +0100

    Refactor pointer validation to use a macro

--- src/utils.h
+++ src/utils.h
@@ -13,16 +13,17 @@
 /**
  * @brief Checks if a pointer is valid and exits the program if it is not.
  *
- * This function verifies that the given pointer is not NULL. If the pointer
+ * This macro verifies that the given pointer is not NULL. If the pointer
  * is NULL, it prints an error message to the standard error stream and
  * terminates the program with an exit status of `EXIT_FAILURE`.
  *
  * @param ptr The pointer to check.
  * @param msg The error message to display if the pointer is NULL.
  */
-static inline void check_ptr(const void *ptr, const char *msg) {
-    if (!ptr) {
-        fprintf(stderr, "Error: %s\n", msg);
-        exit(EXIT_FAILURE);
-    }
-}
+#define CHECK_PTR(ptr, msg) \
+    do { \
+        if (!(ptr)) { \
+            fprintf(stderr, "Error: %s\n", (msg)); \
+            exit(EXIT_FAILURE); \
+        } \
+    } while (0)
